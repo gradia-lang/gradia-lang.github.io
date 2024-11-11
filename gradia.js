e068e97edf4a1b73fe540c747a7734f064bf0b1f
梶塚太智 <kajizukataichi@outlook.jp>
2024-11-11 17:58:40 +0900

    Update gradia.js

--- gradia.js
+++ gradia.js
@@ -1,13 +1,13 @@
 import init, { run_gradia, Gradia } from "./pkg/gradia_wasm.js";
 
 init().then(() => {
-  window.run_gradia = run_gradia;
-  window.Gradia = Gradia;
-  const gradiaElements = document.getElementsByClassName("gradia");
+    window.run_gradia = run_gradia;
+    window.Gradia = Gradia;
 
-  for (let i = 0; i < gradiaElements.length; i++) {
-    const gradiaElement = gradiaElements[i];
-    const code = gradiaElement.innerText.trim();
-    gradiaElement.innerHTML = run_gradia(code);
-  }
+    const gradiaElements = document.getElementsByClassName("gradia");
+    for (let i = 0; i < gradiaElements.length; i++) {
+        const gradiaElement = gradiaElements[i];
+        const code = gradiaElement.innerText.trim();
+        gradiaElement.innerHTML = run_gradia(code);
+    }
 });
